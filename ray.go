4178334be90aa4ed8f9b85c4f98234d0ffeac86b
Pitinari <tomy.pitinari@gmail.com>
2022-05-18 19:49:24 -0300

    ray interface

--- ray.go
+++ ray.go
@@ -1,1 +1,18 @@
 package main
+
+type Ray struct {
+	origin    point3
+	direction Vect3
+}
+
+func ray_origin(ray Ray) point3 {
+	return ray.origin
+}
+
+func ray_direction(ray Ray) Vect3 {
+	return ray.direction
+}
+
+func ray_at(ray Ray, t float64) point3 {
+	return point3(vector_add(Vect3(ray.origin), vector_scalar_mul(ray.direction, t)))
+}
